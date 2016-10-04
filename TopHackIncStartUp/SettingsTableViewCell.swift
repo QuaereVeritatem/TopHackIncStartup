dfc61587b6a5cdb30859909b1d02162188402cf0
QuaereVeritatem <robert.lee.martin@gmail.com>
2016-10-03 20:27:49 -0500

    settings and calendar VC added. new classes added
    settings and calendar VC added, new art for login and registration screen, re did layout for new art screens, added new classes for new viewcontrollers

---
+++ TopHackIncStartUp/SettingsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsTableViewCell.swift
+//  TopHackIncStartUp
+//
+//  Created by Robert Martin on 10/3/16.
+//  Copyright © 2016 Robert Martin. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
